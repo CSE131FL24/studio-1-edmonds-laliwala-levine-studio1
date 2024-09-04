50e1d2690d2bf5725fc976b8549aeda57a5a1f10
daani <daani@DESKTOP-1U1ASV6.dhcp.wustl.edu>
2024-09-04 15:39:01 -0500

    hi

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,16 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		
+		boolean ascending1 = (x > y);
+		boolean ascending2 = (y > z);
+		boolean ascending = (ascending1 && ascending2);
+		
+		boolean descending1 = (x < y);
+		boolean descending2 = (y < z);
+		boolean descending = (descending1 && descending2);
+		
+		System.out.println(ascending || descending);
 
 	}
 
